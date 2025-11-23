8c17dec2fa9ced0a300d6573fa436688d46f5144
ishantpandey <ishantpandey2000@gmail.com>
2025-11-23 16:38:00 +0530

    reverse proxy setup

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
 app.use('/api',router)
 
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log('server started');
